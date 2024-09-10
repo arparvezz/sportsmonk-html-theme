a8fda6be295212d8ea1f0b73aaf37f7ffb216134
arparvezz <arparvez50@gmail.com>
2024-09-10 20:59:05 +0600

    second commit, pages added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("hello from sportsmonk!")
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('#swiper-hero', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -18,22 +18,25 @@ const swiper = new Swiper('.swiper', {
   });
   
 
-  const gallary = new Swiper('#gallary', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 5,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
+new Swiper('#swiper-gallary',{
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 1000,
+  },
+  pagination: {
+    el: '.gallary-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+
+  breakpoints: {
+    // when window width is >= 740px
+    740: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    }
+  }
+})
